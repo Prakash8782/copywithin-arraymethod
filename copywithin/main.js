b87eae83bb95ab1b57aacd1f076ec6f1101baa13
Prakash8782 <prakash8782@gmail.com>
2022-09-12 17:02:08 +0530

    copywithin

---
+++ copywithin/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+colors.copyWithin(2,0);
+console.log("colors : " ,colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.copyWithin(2,0)");
+console.log("change in array : ",colors);
+console.log("return : length of array after copyWithin(); is "+colors.length);
